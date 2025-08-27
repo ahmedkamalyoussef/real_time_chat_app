592dfe4f984e165b1069d7e448a00f140bf23c19
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-08-27 06:01:10 +0300

    add railway.json

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import { protect } from '../middlewares/auth.middleware.js';
-import { getFriends,getMessages,sendMessage,isDoingSomething } from '../controllers/message.controller.js';
+import express from "express";
+import { protect } from "../middlewares/auth.middleware.js";
+import {
+  getFriends,
+  getMessages,
+  sendMessage,
+  isDoingSomething,
+} from "../controllers/message.controller.js";
 const router = express.Router();
 
-router.get('/friends',protect,getFriends);
-router.get('/:userToChatId', protect, getMessages);
-router.post(['/send', '/send/:id'], protect, sendMessage);
-router.post('/doSomeThing/:id',protect, isDoingSomething);
+router.get("/friends", protect, getFriends);
+router.get("/:userToChatId", protect, getMessages);
+// router.post(['/send', '/send/:id'], protect, sendMessage);
+router.post("/send", protect, sendMessage);
+router.post("/send/:id", protect, sendMessage);
+router.post("/doSomeThing/:id", protect, isDoingSomething);
 export default router;
